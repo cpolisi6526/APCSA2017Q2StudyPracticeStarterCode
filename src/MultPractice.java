b82cfa1d8e0b011bba002fd123f34c314b983e39
BT_1N3_30 <d>
2019-04-01 08:26:28 -0400

    finished

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,21 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice {
+    int firstInteger;
+    int secondInteger;
+    public MultPractice(int f, int s){
+        this.firstInteger=f;
+        this.secondInteger=s;
+    }
+
+    @Override
+    public String getProblem() {
+        return firstInteger + " TIMES " + secondInteger;
+    }
+
+    @Override
+    public void nextProblem() {
+        secondInteger++;
+    }
 }
